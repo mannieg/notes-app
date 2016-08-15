4190b71f17bfb15b9c2ec6444c5c442bb76562ec
Manjinder Gill <manjinder.s.gill@googlemail.com>
2016-08-15 16:42:49 +0100

    Added false assert test

--- tests/assert.js
+++ tests/assert.js
@@ -3,9 +3,16 @@
   function isTrue(assertionToCheck) {
     if (!assertionToCheck) {
       throw new Error("Assertion failed: " + assertionToCheck + " is NOT truthy");
-    } 
+    }
+  }
+
+  function isFalse(assertionToCheck) {
+    if(assertionToCheck) {
+      throw new Error("Assertion failed: " + assertionToCheck + " is NOT falsy");
+    }
   }
 
   exports.isTrue = isTrue;
+  exports.isFalse = isFalse;
 
 })(this);
