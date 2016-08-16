7e99f4ddfebe2d15d872c31264292023b37c8629
Manjinder Gill <manjinder.s.gill@googlemail.com>
2016-08-16 18:32:33 +0100

    Refactored code as suggested by Roi

--- src/controllers/note-controller.js
+++ src/controllers/note-controller.js
@@ -1,13 +1,13 @@
 (function(exports){
 
   function NoteController(NoteList, Note, NoteView){
-    this._noteList = new NoteList(Note);
-    this._noteList.storeNote("Favorite drink: seltzer");
-    this._noteView = new NoteView(this._noteList);
+    var noteList = new NoteList(Note);
+    noteList.storeNote("Favorite drink: seltzer");
+    this.noteView = new NoteView(noteList);
   }
 
   NoteController.prototype.printNotes = function(){
-    document.getElementById('app').innerHTML = this._noteView.getHTML();
+    document.getElementById('app').innerHTML = this.noteView.getHTML();
   };
 
   if (typeof module !== 'undefined')
--- src/models/note-list-model.js
+++ src/models/note-list-model.js
@@ -16,5 +16,5 @@
     module.exports = NoteList;
   else
     exports.NoteList = NoteList;
-    
+
 })(this);
