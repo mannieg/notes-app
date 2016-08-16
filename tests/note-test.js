05f61a540fd276e2fa0e0da48ce60eae0ba2fe9d
Ross Hamilton <rosshamilton.dev@gmail.com>
2016-08-16 15:10:11 +0100

    complete task1

--- tests/note-test.js
+++ tests/note-test.js
@@ -1,17 +1,14 @@
-// var exclaim = require("./exclaim");
-//
-// if (exclaim("hi") !== "hi!!!!!") {
-//   throw new Error("Exclaiming hi should equal hi!!!!!");
-// } else {
-//   console.log(".");
-// }
 var assert = require("./assert");
 var Note = require("../models/note-model");
 
-function testNoteHasText() {
+(function(exports) {
   var note = new Note("My favourite language is JavaScript");
 
-  assert.isTrue(note.getText() === "My favourite language is JavaScript");
+  function testNoteHasText() {
+    assert.isTrue(note.getText() === "My favourite language is JavaScript");
+  };
 
-}
-testNoteHasText();
+  exports.testNoteHasText = testNoteHasText;
+})(this);
+
+this.testNoteHasText()
