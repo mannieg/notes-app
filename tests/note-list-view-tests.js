90d7c2b87c33c265e77f88d5117013c4469275b2
Manjinder Gill <manjinder.s.gill@googlemail.com>
2016-08-16 14:13:02 +0100

    Get html note list test passing

--- tests/note-list-view-tests.js
+++ tests/note-list-view-tests.js
@@ -3,15 +3,15 @@ var modules = require("./modules");
 var Note = modules.Note;
 var NoteList = modules.NoteList;
 var NoteView = modules.NoteView;
+var assert = modules.assert;
 
 function testNoteListDisplaysNotes() {
 
   var noteList = new NoteList(Note);
   noteList.storeNote("Pizza");
   var noteView = new NoteView(noteList);
-  var htmlTest = "<li><div>Pizza</div></li>";
-  //assert.isTrue(noteView.getHTML() === htmlTest);
-  console.log(noteView.getHTML());
+  var htmlTest = "<ul><li><div>Pizza</div></li></ul>";
+  assert.isTrue(noteView.getHTML() === htmlTest);
 }
 
 testNoteListDisplaysNotes();
