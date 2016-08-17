b9de3f94c2f8b67b81063bc90e6d01c1c7f283bf
albiebabie <albiebabie@gmail.com>
2016-08-17 14:42:07 +0100

    note list view - no test yet!

---
+++ spec/noteListViewSpec.js
@@ -0,0 +1,13 @@
+// Takes a note list model upon instantiation.
+// Has a method that, when called, returns a string of HTML that represents the
+// note list model. For example: <ul><li><div>Favourite food: pesto</div></li><li>
+// <div>Favourite drink: seltzer</div></li></ul>.
+// Handles a note list model that has no notes, one note or several notes.
+
+describe("Note List View", function() {
+
+  it("takes a note list model upon instantiation", function() {
+    var list = new List();
+
+  });
+});
